7bb56d33839e375b17b56b16338c2594dcc3b239
LouisBortolotto <louis.bortolotto@my.jcu.edu.au>
2019-08-22 11:09:58 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,3 +1,9 @@
 """
 Louis Bortolotto
 """
+MIN_LENGTH = 10
+password = str(input("What is your password?"))
+while len(password) < 10:
+    password = str(input("password must be 10 characters or more"))
+for i in range(len(password)):
+    print("*", end='')
